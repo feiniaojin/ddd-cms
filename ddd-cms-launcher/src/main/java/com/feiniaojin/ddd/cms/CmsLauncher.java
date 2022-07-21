1521517d54c4e160fc51747d7c2e1e915eccb02f
qinyujie3 <qinyujie3@jd.com>
2022-07-21 23:11:58 +0800

    init

--- ddd-cms-launcher/src/main/java/com/feiniaojin/ddd/cms/CmsLauncher.java
+++ ddd-cms-launcher/src/main/java/com/feiniaojin/ddd/cms/CmsLauncher.java
@@ -1,13 +1,9 @@
 package com.feiniaojin.ddd.cms;
 
-import com.feiniaojin.ddd.cms.infrastructure.persitrence.PersistenceConfig;
-import com.feiniaojin.ddd.cms.interfaces.web.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({PersistenceConfig.class, WebConfig.class})
 public class CmsLauncher {
     public static void main(String[] args) {
         SpringApplication.run(CmsLauncher.class, args);
