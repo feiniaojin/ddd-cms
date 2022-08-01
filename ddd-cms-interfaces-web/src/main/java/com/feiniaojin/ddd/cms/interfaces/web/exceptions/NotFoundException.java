1b316c13e8139ccce81b5b0b40575b21e92ecf77
qinyujie3 <qinyujie3@jd.com>
2022-08-01 18:28:59 +0800

    init

--- ddd-cms-interfaces-web/src/main/java/com/feiniaojin/ddd/cms/interfaces/web/exceptions/NotFoundException.java
+++ ddd-cms-interfaces-web/src/main/java/com/feiniaojin/ddd/cms/interfaces/web/exceptions/NotFoundException.java
@@ -1,7 +1,7 @@
 package com.feiniaojin.ddd.cms.interfaces.web.exceptions;
 
 import com.feiniaojin.ddd.cms.domain.article.exceptions.NotFoundDomainException;
-import com.feiniaojin.gracefulresponse.api.ExceptionAliasFor;
+import com.feiniaojin.ddd.ecosystem.gracefulresponse.api.ExceptionAliasFor;
 
 @ExceptionAliasFor(code = "1404", msg = "文章找不到了", aliasFor = NotFoundDomainException.class)
 public class NotFoundException extends RuntimeException {
